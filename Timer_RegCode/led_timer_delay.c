d481654a9f5109cbd32211f4faa10cc0f6e5c7b6
Mahesh K <60285145+Mahesh-7@users.noreply.github.com>
2021-07-17 15:18:42 +0530

    Update led_timer_delay.c

--- Timer_RegCode/led_timer_delay.c
+++ Timer_RegCode/led_timer_delay.c
@@ -1,49 +1,30 @@
 #include "stm32f10x.h"                  // Device header
 
-
-void delay_ms(volatile unsigned int delay)
+void timer2_delay_ms(unsigned int delay)
 {
-			//timer delay =fosc/prec+1
-			//fosc=60mhz  pre=60000-1	
-			//timer2 config			
-	
-			unsigned int i=0;
-	
-			for(i=delay;i>0;i--)
-			{
-				TIM2->EGR = 0x0001; //reset timer2
-				
-				while(TIM2->CNT <1000);//1ms				
-			}	
-			
-		
-
+	RCC->APB1ENR= 1<<0; //timer2 clk enable
+	TIM2->CR1|=1<<0; //timer 2 on CEN=1
+	TIM2->CR1|=1<<7; //timer 2 ARR enable	
+	TIM2->PSC=7999;  //prescale value set
+	TIM2->ARR=delay; 
+	//wait untill UIF flag set(timer2 flag) 	
+	while(!(TIM2->SR &(1<<0))); 
+	TIM2->SR &=0x0000;  //UIF flag reset(timer2 flag)	
 }
-	
-	
 
 int main()
 {
-
 	RCC->APB2ENR  = (1<<4);  //portc clk on
 	RCC->APB2ENR  |= (1<<0);  //alter func clk on
-	
 	//pc13
-	GPIOC->CRH = (3<<20);
-	GPIOC->CRH &= ~(1<<22);	
-	
-	//timer2 config
-	
-	RCC->APB1ENR = (1<<0);  //timer 2clk on	
-	TIM2->PSC = 59;  //1us	
-	TIM2->ARR =0xffff;  
-	TIM2->CR1 = (1<<0);// timer 2 on
-	
-	while(1)
+	GPIOC->CRH = (3<<20);//output 50mhz
+	GPIOC->CRH &= ~(1<<22);	//GPIO push pull mode
+	while (1)
 	{
-		GPIOC->ODR ^= (1<<13);
-		delay_ms(1000);   //1sec 
+		GPIOC->ODR ^= (1<<13);		
+		timer2_delay_ms(2000);	
 	}
-
+	return 0;
 }
+
 
